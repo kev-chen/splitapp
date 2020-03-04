723ebe4fc978d7948935410c923e4065074d9a71
Kevin Chen <kevchen21@gmail.com>
2020-03-04 16:30:37 -0500

    Updates

---
+++ precache-manifest.878476a422dfbf60b4ffe0b5ef70084b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff1e52a48628e7bf57e09a075e98b1a",
+    "url": "/splitapp/index.html"
+  },
+  {
+    "revision": "2c07cb407abbc857fee5",
+    "url": "/splitapp/static/css/main.01ae28b6.chunk.css"
+  },
+  {
+    "revision": "779389b4863d834c8a21",
+    "url": "/splitapp/static/js/2.2e2d749f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/splitapp/static/js/2.2e2d749f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c07cb407abbc857fee5",
+    "url": "/splitapp/static/js/main.3ff75a26.chunk.js"
+  },
+  {
+    "revision": "86e677cb1500bbcc8a0a",
+    "url": "/splitapp/static/js/runtime-main.8424df5b.js"
+  },
+  {
+    "revision": "5fee1e156a2d1d906b0ed2a279481d0b",
+    "url": "/splitapp/static/media/device-promo.5fee1e15.png"
+  },
+  {
+    "revision": "eb5ff08d3f576416e68b479c14e011c9",
+    "url": "/splitapp/static/media/play_store.eb5ff08d.png"
+  }
+]);
