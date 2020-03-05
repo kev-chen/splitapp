5c4ed88fce9bb067670725806138ec19a955e121
Kevin Chen <kevchen21@gmail.com>
2020-03-05 11:36:20 -0500

    Added app store link

--- src/components/StoreBadges/StoreBadges.js
+++ src/components/StoreBadges/StoreBadges.js
@@ -4,9 +4,23 @@ import "./style.css";
 const StoreBadges = props => {
   return (
     <div className="badge-container">
-      <img className="app-store" src={require("../../assets/app_store.png")} alt="App Store" />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://apps.apple.com/us/app/split-simplify-your-hangouts/id1500703560"
+      >
+        <img
+          className="app-store"
+          src={require("../../assets/download-on-app-store.svg")}
+          alt="App Store"
+        />
+      </a>
       <div className="divider" />
-      <img className="play-store" src={require("../../assets/play_store.png")} alt="Play store" />
+      <img
+        className="play-store"
+        src={require("../../assets/play_store.png")}
+        alt="Play store"
+      />
     </div>
   );
 };
