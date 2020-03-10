3a890d009a164537ae791b6bb8a6fd8bf1c748a9
Kevin Chen <kevchen21@gmail.com>
2020-03-09 22:00:38 -0400

    Added play store link

--- src/components/StoreBadges/StoreBadges.js
+++ src/components/StoreBadges/StoreBadges.js
@@ -11,16 +11,22 @@ const StoreBadges = props => {
       >
         <img
           className="app-store"
-          src={require("../../assets/download-on-app-store.svg")}
+          src={require("../../assets/app_store.png")}
           alt="App Store"
         />
       </a>
       <div className="divider" />
-      <img
-        className="play-store"
-        src={require("../../assets/play_store.png")}
-        alt="Play store"
-      />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://play.google.com/store/apps/details?id=com.kchen.split"
+      >
+        <img
+          className="play-store"
+          src={require("../../assets/play_store.png")}
+          alt="Play store"
+        />
+      </a>
     </div>
   );
 };
