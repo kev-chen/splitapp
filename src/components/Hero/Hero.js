47495539d154124243c853def88525812c1fbc70
Kevin Chen <kevchen21@gmail.com>
2020-03-16 17:51:13 -0400

    Mobile-first hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,39 +3,19 @@ import './style.css';
 import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
 
 const Hero = (props) => {
-  // const btn = { backgroundColor: Color.PRIMARY, borderRadius: 0 };
-
-  const renderHeader = () => {
-    if (props.icon) {
-      return (
-        <div>
-          <div className="hero-container">
-            <img className="header-icon" src={props.icon} alt="icon" />
-            <div className="text-left">
-              {props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1>}
-              {props.subtitle && <h3 className="display-4 font-weight-light">{props.subtitle}</h3>}
-              {props.text && <h3 className="lead font-weight-light text-left">{props.text}</h3>}
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          {props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1>}
-          {props.subtitle && <h3 className="display-4 font-weight-light">{props.subtitle}</h3>}
-          {props.text && <h3 className="lead font-weight-light text-left">{props.text}</h3>}
-        </div>
-      );
-    }
-  };
-
   return (
     <Jumbotron className="bg-transparent jumotron-fluid">
       <Container fluid={true}>
         <Row className="justify-content-center py-5">
-          <Col className="text-center" md={12}>
-            {renderHeader()}
+          {props.icon && (
+            <Col className="d-flex justify-content-center" md={3}>
+              <img className="header-icon" src={props.icon} alt="icon" />
+            </Col>
+          )}
+          <Col className={props.icon ? 'text-left' : 'text-center'} md={9}>
+            {props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1>}
+            {props.subtitle && <h3 className="display-4 font-weight-light">{props.subtitle}</h3>}
+            {props.text && <h3 className="lead font-weight-light" id="description">{props.text}</h3>}
           </Col>
         </Row>
       </Container>
