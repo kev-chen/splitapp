dd8d8a45d1c56c0bbae924b585a3bb1798b8f6e5
Kevin Chen <kevchen21@gmail.com>
2020-03-19 23:36:13 -0400

    Updated hero for mobile friendliness

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,22 +4,24 @@ import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
 
 const Hero = (props) => {
   return (
-    <Jumbotron className="p-5 bg-transparent jumbotron-fluid">
+    <Jumbotron className="px-5 pb-0 mb-0 bg-transparent jumbotron-fluid">
       <Container fluid={true}>
         <Row className="justify-content-center py-5">
           {props.icon && (
-            <Col id="image-container" className="text-center my-auto" md={3}>
+            <Col
+              id="image-container"
+              className="mb-5 mb-md-0 align-items-center d-flex justify-content-center justify-content-md-end justify-content-lg-end"
+              xs={{ span: 12, order: 1 }}
+              md={{ span: 3, order: 2 }}>
               <img className="header-icon img-fluid" src={props.icon} alt="icon" />
             </Col>
           )}
-          <Col className={props.icon ? 'text-left' : 'text-center'} md={9}>
+          <Col
+            className={props.icon ? 'text-center text-md-left text-lg-left' : 'text-center'}
+            xs={{ span: 12, order: 2 }}
+            md={{ span: 7, order: 1 }}>
             {props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1>}
-            {props.subtitle && <h3 className="display-4 font-weight-light">{props.subtitle}</h3>}
-            {props.text && (
-              <h3 className="lead font-weight-light" id="description">
-                {props.text}
-              </h3>
-            )}
+            {props.subtitle && <h3 className="display-4 font-weight-lighter">{props.subtitle}</h3>}
           </Col>
         </Row>
       </Container>
